2cdbbe360bab750f7c7532edc45a824abb9a90e5
nidhi.shah <nidhi.shah@ICPL12979.infostretch.com>
2018-08-03 12:48:01 +0530

    Initial Commit

---
+++ java/com/qasdemo/StepsBackLog.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package com.qasdemo;
+
+import com.qmetry.qaf.automation.step.NotYetImplementedException;
+import com.qmetry.qaf.automation.step.QAFTestStep;
+
+/**
+ * @author nidhi.shah
+ */
+public class StepsBackLog {
+
+	/**
+	 * Auto-generated code snippet by QMetry Automation Framework.
+	 */
+	@QAFTestStep(description = "logout")
+	public void logout() {
+		System.out.println("Logout");
+		
+	}
+
+}
